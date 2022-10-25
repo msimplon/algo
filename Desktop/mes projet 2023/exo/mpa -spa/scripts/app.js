de00b4d0d9bdf82afe5ffc57d314980ea8ca2a26
manal benallal <manelbenallal@hotmail.fr>
2022-10-25 12:04:10 +0200

    exp js menu

---
+++ Desktop/mes projet 2023/exo/mpa -spa/scripts/app.js
@@ -0,0 +1,20 @@
+const menu = `
+         <nav>
+            <ul>
+                <li><a href="index.html">home</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <li><a href="sign-in.html">Contact</a></li>
+                <li><a href="about.html">About</a></li>
+            </ul>
+        </nav>
+`;
+
+const header = document.getElementById("header");
+header.innerHTML = menu;
+
+const copyright = "<p>Copyright Simplon 2022</p> ";
+const footer = document.getElementById("footer");
+footer.innerHTML = footer;
+
+console.log(footer);
+
