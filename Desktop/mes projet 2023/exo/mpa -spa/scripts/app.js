63e67f780f19e787d21358e26f2444e52e048f68
manal benallal <manelbenallal@hotmail.fr>
2022-10-26 11:55:35 +0200

    interpolation

--- Desktop/mes projet 2023/exo/mpa -spa/scripts/app.js
+++ Desktop/mes projet 2023/exo/mpa -spa/scripts/app.js
@@ -1,12 +1,28 @@
+//Declare a variable of type object
+// 
+
+const labels = { home: "Home", contact: "contact", signIn: "Sign-in", about: "About" };
+// a json (Javascript Object Notation)
+
+// `` = template literal with expression $(expression)
+// replacing an expression by its value is an interpolation 
 const menu = `
-         <nav>
-            <ul>
-                <li><a href="index.html">home</a></li>
-                <li><a href="contact.html">Contact</a></li>
-                <li><a href="sign-in.html">Contact</a></li>
-                <li><a href="about.html">About</a></li>
-            </ul>
-        </nav>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" href="index.html">${labels.home}</a>
+        <a class="nav-link" href="contact.html">${labels.contact}</a>
+        <a class="nav-link" href="sign-in.html">${labels.signIn}</a>
+        <a class="nav-link" href="about.html">${labels.about}</a>
+      </div>
+    </div>
+  </div>
+</nav>
 `;
 
 const header = document.getElementById("header");
@@ -14,7 +30,7 @@ header.innerHTML = menu;
 
 const copyright = "<p>Copyright Simplon 2022</p> ";
 const footer = document.getElementById("footer");
-footer.innerHTML = footer;
+footer.innerHTML = copyright;
 
 console.log(footer);
 
